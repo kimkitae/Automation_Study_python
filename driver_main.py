edc1a0a9add47ecf671e2a0914bf7e9a4ed47835
impresent89 <dlzkdsnxm1!>
2022-04-06 22:49:40 +0900

    ttest1

--- driver_main.py
+++ driver_main.py
@@ -1,19 +1,22 @@
 from appium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 
 desired_caps = {}
 
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = "8"
+desired_caps['platformVersion'] = "12"
 desired_caps["automationName"] = "uiautomator2"
-desired_caps['uuid'] = "2be02f89ec1d7ece"
-desired_caps['appActivity'] = "kr.co.yogiyo.riderapp.ui.MainActivity"
-desired_caps['appPackage'] = "kr.co.yogiyo.riderapp"
+desired_caps['uuid'] = "R3CN5056PGL"
+desired_caps['appActivity'] = "com.dalkomm.beatorder.presentation.v3.intro.IntroActivity"
+desired_caps['appPackage'] = "com.dalkomm.beatorder.debug"
 desired_caps['unicodeKeyboard'] = "false"
 desired_caps['resetKeyboard'] = "true"
 desired_caps['noReset'] = "true"
 desired_caps['mockLocationApp'] = "com.gsmartstudio.fakegps"
 
-driver = webdriver.Remote('http://localhost:7725/wd/hub', self.desired_caps)
+driver = webdriver.Remote('http://localhost:7725/wd/hub', desired_caps)
 driver.implicitly_wait(5)
+
+time.sleep(5)
