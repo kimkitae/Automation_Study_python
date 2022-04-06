b50b047df9d1b1c9710f6a5ccc73b7fc6d6809a6
김대경 <>
2022-04-06 22:48:38 +0900

    test1

--- driver_main.py
+++ driver_main.py
@@ -5,15 +5,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 desired_caps = {}
 
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = "8"
+desired_caps['platformVersion'] = "10"
 desired_caps["automationName"] = "uiautomator2"
-desired_caps['uuid'] = "2be02f89ec1d7ece"
-desired_caps['appActivity'] = "kr.co.yogiyo.riderapp.ui.MainActivity"
-desired_caps['appPackage'] = "kr.co.yogiyo.riderapp"
+
+desired_caps['appActivity'] = "com.campmobile.noon.ui.splash.SplashActivity"
+desired_caps['appPackage'] = "com.campmobile.noon"
 desired_caps['unicodeKeyboard'] = "false"
 desired_caps['resetKeyboard'] = "true"
 desired_caps['noReset'] = "true"
-desired_caps['mockLocationApp'] = "com.gsmartstudio.fakegps"
 
-driver = webdriver.Remote('http://localhost:7725/wd/hub', self.desired_caps)
+driver = webdriver.Remote('http://localhost:7725/wd/hub', desired_caps)
 driver.implicitly_wait(5)
