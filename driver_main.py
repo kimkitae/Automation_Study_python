f0960cf54b5b94db572bc6aa0e2bd11453243dff
hayul <>
2022-04-10 18:26:56 +0900

    test2

--- driver_main.py
+++ driver_main.py
@@ -1,4 +1,6 @@
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
@@ -14,12 +16,22 @@ desired_caps['appPackage'] = "com.elevenst"
 desired_caps['unicodeKeyboard'] = "false"
 desired_caps['resetKeyboard'] = "true"
 desired_caps['noReset'] = "true"
-desired_caps['mockLocationApp'] = "com.gsmartstudio.fakegps"
+desired_caps['systemPort'] = 8201
+desired_caps['adbPort'] = 5039
 
-driver = webdriver.Remote('http://localhost:7725/wd/hub', desired_caps)
+driver = webdriver.Remote('http://localhost:7726/wd/hub', desired_caps)
 driver.implicitly_wait(5)
 
 
-time.sleep(5)
+driver.find_element(By.ID, "com.elevenst:id/btn_my").click()
+
+time.sleep(2)
+driver.find_element(By.XPATH, "//android.view.View[@content-desc='찜']").click()
+time.sleep(3)
+print(driver.page_source)
+list = driver.find_elements(By.XPATH, "//*[@class='android.widget.ListView' and @index='3']/android.view.View")
+time.sleep(2)
+list[0].click()
+time.sleep(2)
 
 
